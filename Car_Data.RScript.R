dc6c8c4955171cffb143e2c189a7307156fe111f
NataliaVelasquez18 <nataliavelasquez@natalias-mbp.lan>
2021-03-09 17:05:02 -0600

    adding second analysis

--- Car_Data.RScript.R
+++ Car_Data.RScript.R
@@ -1,5 +1,14 @@
 library(dplyr)
+
+## Performance
 mechacar_dataframe <- read.csv(file = 'MechaCar_mpg.csv', check.names = F, stringsAsFactors = F)
 #perform linear regression
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance,data=mechacar_dataframe)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance,data=mechacar_dataframe))
+
+## Suspension Coil
+suspension_coil_dataframe <- read.csv(file = 'Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+
+total_summary <- suspension_coil_dataframe %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+
+lot_summary <- suspension_coil_dataframe %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
